85b578c8f7c699828c5125f0c0f87d63843b7aa9
patriciaCastillo <patrycast81@gmail.com>
2024-10-10 23:47:30 -0300

    modificacion del carrito

--- js/index.js
+++ js/index.js
@@ -4,12 +4,22 @@ const categories= document.querySelector(".categories");
 const categoryList= document.querySelectorAll(".category");
 
 
+const carritoBtn=document.querySelector(".carrito-container"); //carrito
+const  cartModal=document.querySelector(".card-carrito");  //modal
+const menuBtn = document.querySelector(".menu-label"); //menu hamburguesa
+const menu= document.querySelector(".navbar-ul"); //ul
+
+const productCard=document.querySelector(".card-container");
+const overlay= document.querySelector(".overlay");
+const total= document.querySelector(".total")
+
+let cart=[];
 
 
 
 //template
 const createProductTemplate=(producto)=>{
-    const {name, img, price, txt}= producto
+    const {id,name, img, price, txt}= producto
     return `
     <div class="muebles__container">
         <div class="card__muebles">
@@ -18,7 +28,12 @@ const createProductTemplate=(producto)=>{
             </div>
             <p>${name}</p>
             <span>U$D ${price}</span>
-            <button class"btn-carrito">${txt}</button>
+            <button class="btn-carrito"
+            data-id='${id}'
+            data-name='${name}'
+            data-price='${price}'
+            data-img='${img}'
+            >${txt}</button>
         </div>        
     </div`
 
@@ -66,6 +81,11 @@ const changeFilter=(btn)=>{
     appState.activeFilter=btn.dataset.category
     changeColorActive(appState.activeFilter)
     console.log(appState)
+    if(!appState.activeFilter){
+        showMoreBtn.classList.remove("hidden");
+        return;
+    }
+    showMoreBtn.classList.add("hidden");
 }
 
 const addFilter=(e)=>{
@@ -78,15 +98,85 @@ const addFilter=(e)=>{
         const filteredProducts=productsData.filter((product)  => 
             product.Category === appState.activeFilter)
             renderProducts(filteredProducts);
-            // appState.currentProductIndex=0;
             return;
         }
         renderProducts(appState.products[0])
 }
 
+
+
+
+//logica menu
+const switchMenu = () => {
+    menu.classList.toggle("open-menu");
+
+    if (cartModal.classList.contains("open-cart")) {
+        cartModal.classList.remove("open-cart");
+        return;
+    }
+    overlay.classList.toggle("overlayActive")
+}
+
+
+//  carrito
+const switchCart = () => {
+    cartModal.classList.toggle("open-cart");
+    if (menu.classList.contains("open-menu")) {
+        menu.classList.remove("open-menu"); 
+        return;
+    }
+    overlay.classList.toggle("overlayActive")
+}
+
+const closeMenuCart= ()=>{
+    if(menu.classList.contains("open-menu") || cartModal.classList.contains("open-cart")){
+        menu.classList.remove("open-menu")
+        cartModal.classList.remove("open-cart")
+        overlay.classList.remove("overlayActive")
+    }
+}
+
+//logica carrito
+
+const addMueble= (e) =>{
+    if(!e.target.classList.contains("btn-carrito")) return;
+    const mueble= dataMueble(e.target.dataset)
+
+    if( isProductInCart(mueble)){
+        appendUnitToProduct(mueble)
+    }else{
+        cart=[...cart, {...mueble, quantity: 1}]}
+        console.log(cart)
+    }
+
+
+const appendUnitToProduct= (mueble)=>{
+    cart=cart.map((cartProduct)=> cartProduct.id === mueble.id
+    ? {...cartProduct, quantity: cartProduct.quantity+1}
+    : cartProduct)
+}    
+
+const dataMueble=(mueble)=>{
+    const {id, name, price, img}= mueble;
+    return {id, name, price, img}
+}   
+
+const isProductInCart = (mueble) => {
+    return cart.find((item) => item.id === mueble.id);
+}
+
+
+
+
 const init=()=>{
     renderProducts(appState.products[0]);
     showMoreBtn.addEventListener("click", showMoreProducts);
-    categories.addEventListener("click", addFilter)
+    categories.addEventListener("click", addFilter);
+    carritoBtn.addEventListener("click", switchCart);
+    menuBtn.addEventListener("click", switchMenu);
+    overlay.addEventListener("click", closeMenuCart);
+    window.addEventListener("scroll", closeMenuCart);
+
+    muebles.addEventListener("click", addMueble);
 }
 init()
